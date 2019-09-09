8c8ff3efff593423b3efa6c653e9a5d612cb284d
simsir-lin <15986907592@163.com>
2019-09-09 12:52:41 +0800

    docs(新增使用案例)

--- example/app.js
+++ example/app.js
@@ -1,5 +1,13 @@
+const Mock = require('wechat-miniprogram-mock')
+const mock = new Mock({
+  ignore: [],
+  delay: 1000 // 延迟1000ms
+})
+mock.add('https://a.com/login', 'post', { userid: 1, username: 'simsir-lin' }, {}, 200)
+mock.start()
+
 App({
   onLaunch: function () {
-    
+
   },
 })
--- example/pages/index/index.js
+++ example/pages/index/index.js
@@ -2,5 +2,21 @@ Page({
   data: {
   },
   onLoad: function (options) {
+  },
+  send() {
+    wx.showLoading({
+      title: '请求中',
+    })
+    wx.request({
+      url: 'https://a.com/login',
+      method: 'post',
+      success: (res) => {
+        wx.hideLoading()
+        wx.showModal({
+          title: '结果',
+          content: `用户名：${res.data.username}`,
+        })        
+      }
+    })
   }
 })
